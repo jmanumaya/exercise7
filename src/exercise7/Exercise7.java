685959b1f1cb4a0eaac823b41becac5017ec26c7
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-09-20 23:30:05 +0200

    Nuevos cambios de prueba

--- src/exercise7/Exercise7.java
+++ src/exercise7/Exercise7.java
@@ -8,7 +8,15 @@ public class Exercise7 {
 		System.out.println("     *    *  *   *  *");
 		System.out.println("     *    *    *    *");
 		System.out.println("*    *    *         *");
-		System.out.println("******    *         *");
+		System.out.println("******    *         *\n");
+		
+		System.out.print("¡¡¡Si ves esto es porque estoy probando a hacer cambios desde mi casa al repositorio!!!\n");
+		
+		int a = 123;
+		int b = 241;
+		String name = "123";
+		
+		System.out.print((a + b) + name);
 
 	}
 
